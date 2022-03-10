e63b23ae4f42d554a45a2960bca21457188bbde4
noman081 <abdullahalnoman642@gmail.com>
2022-03-10 16:33:27 +0600

    After email validation

---
+++ app.js
@@ -0,0 +1,14 @@
+const validateEmail = () => {
+    const email = document.getElementById('email');
+    const emailText = email.value;
+    const regEx = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    const isValid = regEx.test(emailText);
+    if (isValid) {
+        document.getElementById('success').style.display = 'block';
+        document.getElementById('error').style.display = 'none';
+    }
+    else {
+        document.getElementById('error').style.display = 'block';
+        document.getElementById('success').style.display = 'none';
+    }
+}
